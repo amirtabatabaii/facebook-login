e31f41c93ead99ed4bda59cc5e1bde41440e1219
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2020-07-09 12:23:25 +0300

    facebook login

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Facebook from "./Components/Facebook";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Facebook Auth Example</h1>
+        <p>To get started, authenticate with Facebook.</p>
+        <Facebook />
+      </header>
+    </div>
+  );
+}
+
+export default App;
